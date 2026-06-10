7fa636c6cd1e72581982433d7e130178b7ee6444
Samarth Talawar <samarthpt216@gmail.com>
2026-06-10 23:35:54 +0530

    started with encrypt and decrypt subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "lb")]
+#[command(version = "0.1.0")]
+#[command(about = "File Encryption Tool")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Encrypt {
+        file: String,
+    },
+    Decrypt {
+        file: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+
+let cli = Cli::parse();
+
+match cli.command {
+    Commands::Encrypt { file } => {
+        println!("Encrypting {}", file);
+    }
+
+    Commands::Decrypt { file } => {
+        println!("Decrypting {}", file);
+    }
+}
+
 }
